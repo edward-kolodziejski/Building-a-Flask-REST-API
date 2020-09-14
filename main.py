c7afa9c8089950218d133dc1c3032e421d9de17e
edward-kolodziejski <edward.kolodziejski@pm.me>
2020-09-14 16:36:04 +0200

    request parser has been initiated

--- main.py
+++ main.py
@@ -1,17 +1,25 @@
 from flask import Flask
-from flask_restful import Api, Resource
+from flask_restful import Api, Resource, reqparse
 
 app = Flask(__name__)
 api = Api(app)
 
-class HelloWorld(Resource):
-    def get(self):
-        return {"data" : "Hello World"}
-    
-    def post(self):
-        return {"data" : "Posted"}
+video_put_args = reqparse.RequestParser()
+video_put_args.add_argument("name", type=str, help="Name of the video")
+video_put_args.add_argument("views", type=int, help="Views of the video")
+video_put_args.add_argument("likes", type=int, help="Likes of the video")
 
-api.add_resource(HelloWorld, "/helloworld")
+videos = {}
+
+class Video(Resource):
+    def get(self, video_id):
+        return videos[video_id]
+
+    def put(self, video_id):
+        args = video_put_args.parse_args()
+        return {video_id: args}
+
+api.add_resource(Video, "/video/<int:video_id>")
 
 if __name__ == '__main__':
     app.run(debug=True) #debug=True never in prod env 
--- test.py
+++ test.py
@@ -2,6 +2,6 @@ import requests
 
 BASE = "http://127.0.0.1:5000/"
 
-response  = requests.get(BASE + "helloworld")
+response  = requests.put(BASE + "video/1", {"likes":10}) #{} getting extra data not visible in url
 print(response.json())
 
