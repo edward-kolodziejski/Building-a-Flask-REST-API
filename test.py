1689f773e385aaf58474470ec968c46ec85cf8f5
edward-kolodziejski <edward.kolodziejski@pm.me>
2020-09-18 09:57:36 +0200

    get and put method updated; delete method created

--- test.py
+++ test.py
@@ -2,6 +2,9 @@ import requests
 
 BASE = "http://127.0.0.1:5000/"
 
-response  = requests.put(BASE + "video/1", {"likes":10}) #{} getting extra data not visible in url
+# response  = requests.put(BASE + "video/1", {"likes":10, "name":"Tim", "views":1000000}) #{} getting extra data not visible in url
+# print(response.json())
+# input()
+response  = requests.get(BASE + "video/6")
 print(response.json())
 
