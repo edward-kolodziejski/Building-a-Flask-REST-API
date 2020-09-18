a09a196495cda5141f0089d9d0f241e7295a206b
edward-kolodziejski <edward.kolodziejski@pm.me>
2020-09-18 10:24:50 +0200

    video model  created; connected db with sqlalchemy

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
 from flask import Flask
-from flask_restful import Api, Resource, reqparse, abort
+from flask_restful import Api, Resource, reqparse, abortfrom flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 api = Api(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+db = SQLAlchemy(app)
+
+class VideoModel(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(dbString(100), nullable=False) #nullable - it has to have some information
+    views = db.Column(db.Integer, nullable=False)
+    likes = db.Column(db.Integer, nullable=False)
+
+    def __repr__(self):
+        return f"Video(name={name}, views={views}, likes={likes})"
+db.create_all()
 
 video_put_args = reqparse.RequestParser()
 video_put_args.add_argument("name", type=str, help="Name of the video", required=True)
--- test.py
+++ test.py
@@ -2,9 +2,19 @@ import requests
 
 BASE = "http://127.0.0.1:5000/"
 
-# response  = requests.put(BASE + "video/1", {"likes":10, "name":"Tim", "views":1000000}) #{} getting extra data not visible in url
-# print(response.json())
-# input()
-response  = requests.get(BASE + "video/6")
+data= [{"likes":10, "name":"How it is made?", "views":1000000},
+        {"likes":240, "name":"Tim", "views":700000},
+        {"likes":65, "name":"Tim", "views":900}]
+
+for i in range(len(data)):
+    response  = requests.put(BASE + "video/" + str(i), data[i])
+    print(response.json())
+
+input()
+response = requests.delete(BASE + "video/0")
+print(response)
+
+input()
+response  = requests.get(BASE + "video/2")
 print(response.json())
 
