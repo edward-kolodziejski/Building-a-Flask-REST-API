bd508d12d7edd53cedae1077f896d48510b333ec
edward-kolodziejski <edward.kolodziejski@pm.me>
2020-09-21 10:44:21 +0200

    adding entries to db

--- main.py
+++ main.py
@@ -32,12 +32,22 @@ resource_fields = {
 class Video(Resource):
     @marshal_with(resource_fields)
     def get(self, video_id):
-        result = VideoModel.query.get(id=video_id)
+        result = VideoModel.query.filter_by(id=video_id).first()
+        if not result:
+            abort(404, message="Could not find video with that id")
         return result
 
+    @marshal_with(resource_fields)
     def put(self, video_id):
-
-        return videos[video_id], 201 # it has been created
+        args = video_put_args.parse_args()
+        result = VideoModel.query.filter_by(id=video_id).first()
+        if result:
+            abort(409, message="Video id taken...")
+    
+        video = VideoModel(id=video_id, name=args['name'], views=args['views'], likes=args['likes'])
+        db.session.add(video)
+        db.session.commit()
+        return video, 201 # it has been created
 
     def delete(self, video_id):
         abort_if_video_id_doesnt_exist(video_id)
--- test.py
+++ test.py
@@ -11,10 +11,6 @@ for i in range(len(data)):
     print(response.json())
 
 input()
-response = requests.delete(BASE + "video/0")
-print(response)
-
-input()
-response  = requests.get(BASE + "video/2")
+response  = requests.get(BASE + "video/6")
 print(response.json())
 
