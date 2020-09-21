7fc7a2865bf6612691b8a799588bff53a10d78b7
edward-kolodziejski <edward.kolodziejski@pm.me>
2020-09-21 10:58:33 +0200

    handling update request

--- test.py
+++ test.py
@@ -2,15 +2,6 @@ import requests
 
 BASE = "http://127.0.0.1:5000/"
 
-data= [{"likes":10, "name":"How it is made?", "views":1000000},
-        {"likes":240, "name":"Tim", "views":700000},
-        {"likes":65, "name":"Tim", "views":900}]
-
-for i in range(len(data)):
-    response  = requests.put(BASE + "video/" + str(i), data[i])
-    print(response.json())
-
-input()
-response  = requests.get(BASE + "video/6")
+response  = requests.patch(BASE + "video/2", {"views":101})
 print(response.json())
 
